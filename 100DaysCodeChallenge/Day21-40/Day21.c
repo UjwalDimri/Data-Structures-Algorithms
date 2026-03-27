41cb75f53efa0159e1cfef8e7dd98202fe60299a
UjwalDimri <ujwaldimri223@gmail.com>
2026-03-27 19:50:41 +0530

    Create and Traverse Singly Linked List

---
+++ 100DaysCodeChallenge/Day21-40/Day21.c
@@ -0,0 +1,52 @@
+// Problem: Create and Traverse Singly Linked List
+
+// Input:
+// - First line: integer n
+// - Second line: n space-separated integers
+
+// Output:
+// - Print the result
+
+// Example:
+// Input:
+// 5
+// 10 20 30 40 50
+
+// Output:
+// 10 20 30 40 50
+#include<stdio.h>
+#include<stdlib.h>
+struct node {
+    int data;
+    struct node *next;
+};
+struct node *head = NULL;
+int n ;
+int main(){
+    int i = 0;
+    scanf("%d",&n);
+    while(i<n){
+        struct node *new1;
+        new1=(struct node*)malloc(sizeof(struct node));
+        scanf("%d",&new1->data);
+        if(head==NULL){
+            head=new1;
+        }
+        else{
+            struct node *temp;
+            temp=head ;
+            while(temp->next!=NULL){
+                temp=temp->next;
+            }
+            temp->next=new1;
+        }
+        i++;
+    }
+    struct node *temp1 = head;
+    while(temp1!=NULL){
+        printf("%d\t",temp1->data);
+        temp1=temp1->next;
+    }
+    temp1=NULL;
+    free(temp1);
+}
